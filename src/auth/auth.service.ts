864f95a8cb833ccdf4786e69cee06bfcd0d6a079
Salaudeen Ahmad <ahmodadeola@gmail.com>
2022-07-24 23:56:10 +0100

    added jwt setup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,16 @@ import { AuthDto } from 'src/dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable({})
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
   async login(dto: AuthDto) {
     const { email, password } = dto;
     //   check if user exists
@@ -21,9 +27,12 @@ export class AuthService {
     const pwMatch = await argon.verify(user.hash, password);
     if (!pwMatch) throw new ForbiddenException('Credentials incorect');
 
-    // return login details with token
+    // create login token
     const { hash: _, ...userData } = user;
-    return userData;
+    const token = await this.signToken(userData.id, userData.email);
+
+    // return login details with token
+    return { userData, token };
   }
 
   async register(dto: AuthDto) {
@@ -43,9 +52,10 @@ export class AuthService {
       const { hash: p, ...user } = await this.prisma.user.create({
         data: { email, hash },
       });
+      const token = await this.signToken(user.id, user.email);
 
       // return data
-      return user;
+      return { user, token };
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError)
         if (error.code === 'P2002') {
@@ -53,5 +63,20 @@ export class AuthService {
         }
       throw Error;
     }
+  }
+
+  signToken(userId: number, email: string): Promise<string> {
+    //   construct jwt payload
+    const jwtSecret = this.config.get('JWT_SECRET');
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    // return hashed data in a promise
+    return this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: jwtSecret,
+    });
   }
 }
