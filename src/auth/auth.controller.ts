67391f1b34dd5b2bafd216b24ea5c578b9ad3b71
Salaudeen Ahmad <ahmodadeola@gmail.com>
2022-07-24 22:44:18 +0100

    basic auth features

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthDto } from 'src/dto';
 import { AuthService } from './auth.service';
 
 @Controller('api/auth')
@@ -6,12 +7,12 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  async login() {
-    return this.authService.login();
+  async login(@Body() dto: AuthDto) {
+    return this.authService.login(dto);
   }
 
   @Post('signup')
-  async signup() {
-    return this.authService.register();
+  async signup(@Body() dto: AuthDto) {
+    return this.authService.register(dto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { AuthDto } from 'src/dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable({})
 export class AuthService {
-  async login() {
-    return 'This is the login page';
+  constructor(private prisma: PrismaService) {}
+  async login(dto: AuthDto) {
+    const { email, password } = dto;
+    //   check if user exists
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+
+    // compare password with hash
+    const pwMatch = await argon.verify(user.hash, password);
+    if (!pwMatch) throw new ForbiddenException('Credentials incorect');
+
+    // return login details with token
+    const { hash: _, ...userData } = user;
+    return userData;
   }
 
-  async register() {
-    return 'This is the signup page';
+  async register(dto: AuthDto) {
+    const { email, password } = dto;
+    try {
+      //   check if email exist in db
+      const emailUser = await this.prisma.user.findUnique({
+        where: {
+          email,
+        },
+      });
+
+      // hash password
+      const hash: string = await argon.hash(password);
+
+      // save data in db
+      const { hash: p, ...user } = await this.prisma.user.create({
+        data: { email, hash },
+      });
+
+      // return data
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError)
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      throw Error;
+    }
   }
 }
